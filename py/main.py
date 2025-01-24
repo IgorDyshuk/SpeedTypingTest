da1696354de3a82cf838472e5364a66f6007c866
Igor_Dyshuk <igordysuk@gmail.com>
2025-01-24 21:35:12 +0200

    debug auto login to account after page reload

--- py/main.py
+++ py/main.py
@@ -1,11 +1,13 @@
 from datetime import timedelta, datetime
 
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, HTTPException, Response, Depends, Request
+from fastapi import FastAPI, HTTPException, Response, Depends, Request, status, Header
+from jose import jwt
 from pydantic import BaseModel, EmailStr
 import psycopg2
 from authx import AuthX, AuthXConfig
 from passlib.context import CryptContext
+from typing import Optional
 
 app = FastAPI()
 
@@ -97,15 +99,15 @@ def login_user(user: LoginUserSchema, response: Response):
       if not verify_password(user.password, db_user_password):
         raise HTTPException(status_code=401, detail="Incorrect email or password")
 
-      token = security.create_access_token(uid=str(db_user[0]), expires_delta=timedelta(minutes=30))
+      token = security.create_access_token(uid=str(db_user[0]))
       response.set_cookie(
         config.JWT_ACCESS_COOKIE_NAME,
         token,
         httponly=True,
         samesite="none",
         secure=True,
-        max_age=1800,
-        expires=1800,
+        max_age=30,
+        expires=30,
       )
 
       username = db_user[1]
@@ -121,18 +123,26 @@ def login_user(user: LoginUserSchema, response: Response):
 
 
 @app.get("/profile")
-def protected(token=Depends(security.access_token_required)):
-  user_id = token.sub
-  connection = get_db_connection()
+def protected(request: Request):
+  token = request.cookies.get(config.JWT_ACCESS_COOKIE_NAME)
+  if not token:
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
   try:
+    decoded_token = security._decode_token(token)
+    user_id = decoded_token.sub
+
+    connection = get_db_connection()
     with connection.cursor() as cursor:
       cursor.execute("SELECT name FROM public.users WHERE id = %s", (user_id,))
       db_user = cursor.fetchone()
       if not db_user:
-        raise HTTPException(status_code=404, detail="User not found")
-      name = db_user
-      return {
-        "username": name,
-      }
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+      name = db_user[0]
+      return {"username": name}
+
+  except Exception as e:
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
   finally:
     connection.close()
