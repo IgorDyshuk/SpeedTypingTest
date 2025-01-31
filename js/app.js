77e33a3797429e058684b08343c71e4880d0c830
Igor_Dyshuk <igordysuk@gmail.com>
2025-01-31 13:39:55 +0200

    debug red underline for incorrect word (underline shifts bottom line)

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ loginBtn.addEventListener("click", () => {
 
 document.querySelectorAll(".input-container .toggle-password").forEach(toggle => {
   toggle.addEventListener("click", function () {
-    const passwordField = this.previousElementSibling; 0
+    const passwordField = this.previousElementSibling; 
 
     const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
     passwordField.setAttribute("type", type);
