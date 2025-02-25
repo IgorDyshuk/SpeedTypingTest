6114988af128a2f0d3989d3a1e2c89c5bde3c5c0
Igor_Dyshuk <igordysuk@gmail.com>
2025-02-25 19:58:28 +0200

    tried to add site to servers

--- js/origin.js
+++ js/origin.js
@@ -1,2 +1,2 @@
 // export const origin = "http://127.0.0.1:8000"
-export const origin = "https://39ec-62-80-165-66.ngrok-free.app"
+export const origin = "https://crappie-warm-squirrel.ngrok-free.app"
